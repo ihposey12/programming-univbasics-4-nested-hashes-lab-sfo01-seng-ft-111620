594fa94e7b6a111fc1659346eadd68621197baf2
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-26 08:36:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] = {
+programmer_hash[:yukihiro_matsumoto][0] = {
     known_for: "Ruby",
     languages: ["LISP", "C"]
   }
